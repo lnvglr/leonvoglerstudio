e7fadb2d3c1c5af9e1b8f7dcbcee981f51d50ffe
Leon Vogler <leonfvogler@gmail.com>
2024-05-28 09:24:00 +0100

    remove unused watcher

--- app.vue
+++ app.vue
@@ -3,10 +3,10 @@
     <div
       class="inset-0 fixed -z-10 bg-[radial-gradient(circle_at_left,_var(--tw-gradient-stops))] via-20% to-50% to-white bg-cover duration-1000"
       :class="{
-        'from-purple-200 via-blue-50': name === 'contact-about',
+        'from-purple-200 via-blue-50': route.name === 'contact-about',
         'from-red-200 via-orange-50':
-          name === 'privacy-policy' || name === 'legal-notice',
-        'from-teal-200 via-green-50': name === 'index',
+          route.name === 'privacy-policy' || route.name === 'legal-notice',
+        'from-teal-200 via-green-50': route.name === 'index',
       }"
       :style="`filter: hue-rotate(${hue}deg);`"
     />
@@ -26,18 +26,14 @@ import { onMounted } from 'vue'
 
 const route = useRoute()
 const router = useRouter()
-const name = ref('')
 const hue = ref(Math.floor(Math.random() * 360))
 const interval = ref()
+
 onMounted(async () => {
   await router.isReady()
-  name.value = route.name
   interval.value = setInterval(() => hue.value++, 1000)
 })
 onBeforeUnmount(() => {
   clearInterval(interval.value)
-})
-watch(route, () => {
-  name.value = route.name
 })
 </script>
